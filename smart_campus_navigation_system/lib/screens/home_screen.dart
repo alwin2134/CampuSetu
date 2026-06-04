07e58abe0fe42dba6a3a7b0c7f2cbfde1b5101ac
alwin2134 <alwinmathew2134@gmail.com>
2026-06-04 23:22:41 +0530

    Fix syntax errors with latlong2 and pubspec yaml

--- smart_campus_navigation_system/lib/screens/home_screen.dart
+++ smart_campus_navigation_system/lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:vibration/vibration.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
